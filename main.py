ba7b0f76b2a74489ae46c7dd3fa842542374d6fc
areeeeb <39137400+areeeeb@users.noreply.github.com>
2018-11-06 00:00:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,75 +1,70 @@
 from PIL import Image
-import pytesseract, pyautogui, os, threading, time,  sched
+import pytesseract
+import pyautogui
+import os
+import time
 
-def autoShutdown():
-    '''This function automatically shutdowns the computer if the
-        game from steam has finished downloading'''
 
-    #make the ss object
-    ss = pyautogui.screenshot()
+def auto_shutdown():
+	# This function automatically shutdowns the computer if the game from steam has finished downloading
 
-    #area for half the screen we need
-    area = (round(x/2), 0, x, y)
-    ssCroped = Image.Image.crop(ss, area)
+	# make the ss object
+	ss = pyautogui.screenshot()
 
-    if debug == True:
-        ssCroped.save(directory + r"\outCroped.png")
-        ss.save(directory + r"\out.png")
+	# area for half the screen we need
+	area = (round(x / 2), 0, x, y)
+	ss_croped = Image.Image.crop(ss, area)
 
-    #convert the ss object into string
-    string = pytesseract.image_to_string(ssCroped)
-    if debug == True:
-        print(string)
+	if debug is True:
+		ss_croped.save(directory + r"\outCroped.png")
+		ss.save(directory + r"\out.png")
 
-    #check if the required text is in the string
+	# convert the ss object into string
+	string = pytesseract.image_to_string(ss_croped)
+	if debug is True:
+		print(string)
 
-    if 'Play' in string:
-        if debug == True:
-            print("Program works!!")
+	# check if the required text is in the string
 
-        else:
-            os.system('shutdown -s')
+	if 'Play' in string:
+		if debug is True:
+			print("Program works!!")
 
-    #if not call the function it self after 5 minutes
-    else:
-        time.sleep(300)
-        autoShutdown()
-    if debug == True:
-        input("DEBUG COMPLETED. PRESS ENTER TO CONTINUE...")
+		else:
+			os.system('shutdown -s')
+
+	# if not call the function it self after 5 minutes
+	else:
+		time.sleep(300)
+		auto_shutdown()
+	if debug is True:
+		input("DEBUG COMPLETED. PRESS ENTER TO CONTINUE...")
+
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 
-
-#taking input the from user
-while True:
-    try:
-        x, y = input("Enter your reslotion in this format *1366x768*: ").split('x')
-        break
-    except:
-        print("Wrong input! please enter your resolution in required format..")
+# taking input the from user
+x, y = input("Enter your reslotion in this format *1366x768*: ").split('x')
 
 while True:
-    try:
-        timeInMinutes = float(input("Enter the estimated time (in minutes) left for the game to be downloaded: "))
-        break
-    except:
-        print("Wrong input. Please input an integer (in minutes):")
-
-
+	try:
+		time_in_min = float(input("Enter the estimated time (in minutes) left for the game to be downloaded: "))
+		break
+	except TypeError:
+		print("Wrong input. Please input an integer (in minutes):")
 
 x = int(x) - 1
 y = int(y) - 1
 
-timeInSec = timeInMinutes * 60
+time_in_sec = time_in_min * 60
 
 debug = False
 
-if timeInMinutes == 696969:
-    debug = True
-    print("NOW IN DEBUG MODE!!!")
-    directory = input(r'Input the directory path in which you want to save the debugged screenshots like **D:\anyfolder**: ')
-    timeInSec = float(input("Enter the time in seconds: "))
+if time_in_min is 696969:
+	debug = True
+	print("NOW IN DEBUG MODE!!!")
+	directory = input(r'Directory in which you want to save the debugged screenshots like **D:\anyfolder**: ')
+	time_in_sec = float(input("Enter the time in seconds: "))
 
-time.sleep(timeInSec)
-autoShutdown()
-
+time.sleep(time_in_sec)
+auto_shutdown()
